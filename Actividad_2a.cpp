341f657eb5b7a6d0ab5f8c75c7b0c42043c32e34
Sebastian Monti <88687253+SebastianMonti@users.noreply.github.com>
2026-09-03 12:42:31 -0300

    corregida act 2a

--- Actividad_2a.cpp
+++ Actividad_2a.cpp
@@ -1,26 +1,21 @@
-//actividad 2 a
 #include <Arduino.h>
 
-// Guardamos el último estado. En PULLUP, arranca en 1.
-uint8_t estadoAnterior = 1; 
-
+// //actividad 2 a
 void setup() {
-    // 1. Configurar pines y activar PULLUP interno
-    DDRD &= ~(1 << 2);   // Pin 2 Entrada
-    PORTD |= (1 << 2);   // Pin 2 PULLUP
-    DDRB |= (1 << 2);    // Pin 10 Salida (LED)
+    // Configurar Pin 2 (PD2) como ENTRADA (0)
+    DDRD &= ~(1 << 2);
+    
+    // Configurar Pin 10 (PB2) como SALIDA (1)
+    DDRB |= (1 << 5);
 }
 
 void loop() {
-    // 2. Leemos el pin 2 lo más rápido que puede el microcontrolador
-    uint8_t lecturaActual = (PIND & (1 << 2)) ? 1 : 0;
-
-    // 3. Detectamos el flanco de bajada: 
-    // ¿Acaba de leer un 0 (presionado) pero antes estaba en 1 (reposo)?
-    if (lecturaActual == 0 && estadoAnterior == 1) {
-        
-        // Ejecutamos la acción única: Invertir el LED
-        PORTB ^= (1 << ); 
-        
+    // Escuchar el estado del pin 2 usando el registro PIND
+    if (PIND & (1 << 2)) {
+        // Si la lectura es verdadera (Nivel ALTO / 1), encendemos el LED
+        PORTB |= (1 << 5);
+    } else {
+        // Si la lectura es falsa (Nivel BAJO / 0), apagamos el LED
+        PORTB &= ~(1 << 5);
     }
 }
